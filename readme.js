69b33bdfe63808438bac255142f4f5200516734f
Dina Saunders <dina_saunders@mac.com>
2020-10-10 16:11:10 -0400

    prompts added for user input

--- readme.js
+++ readme.js
@@ -2,7 +2,63 @@ var inquirer = require('inquirer');
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {
+        type: "input",
+        message: "Enter your project title:",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Enter your project description:",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "Enter your installation instructions:",
+        name: "Installation"
+
+    },
+    {
+        type: "input",
+        message: "Enter your usage information:",
+        name: "Usage"
+
+    },
+    {
+        type: "input",
+        message: "Enter your contribution guidelines:",
+        name: "Contributing"
+
+    },
+    {
+        type: "input",
+        message: "Enter your test instructions:",
+        name: "Tests"
+
+    },
+    {
+        type: "list",
+        message: "Choose a license for your applicaiton:",
+        name: "License",
+        choices: [
+          "ISC",
+          "MIT",
+          "Apache"
+        ]
+      },
+      {
+          type: "input",
+          message: "What is your GitHub username?",
+          name: "GitHub"
+  
+      },
+      {
+          type: "input",
+          message: "What is your email address?",
+          name: "Email"
+  
+      }
+
   ])
   .then(answers => {
     // Use user feedback for... whatever!!
