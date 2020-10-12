c0f59df4a5a3a37c3c2255f289632e22b41b2978
Dina Saunders <dina_saunders@mac.com>
2020-10-12 16:05:15 -0400

    updated license badge to correctly display in generated md file

--- generated-README.md
+++ generated-README.md
@@ -1,35 +1,33 @@
 
-      # This is my project title
-
-      ## Description
-      This is my project description
-      
-      ## Table of Contents
-      1. [Title](#Title)
-      2. [Description](#Description)
-      3. [Installation](#Installation)
-      4. [Usage](#Usage)
-      5. [License](#License)
-      6. [Contributing](#Contributing)
-      7. [Tests](#Tests)
-      8. [Questions?](#Questions?)
-      
-      ## Installation
-      These are my installation instructions
-      
-      ## Usage
-      Here is the usage information
-      
-      ## License "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-      This application is covered under the [MIT License](https://opensource.org/licenses/MIT)
-      
-      ## Contributing
-      These are the contribution guidelines
-
-      ## Tests
-      Here are the test instructions
-      
-      ## Questions?
-      Email me at me@me.com
-      Check out my [GitHub Profile](https://github.com/GitHubUser) 
+# My Project
+## Description
+Desc
+  
+## Table of Contents
+1. [Title](#Title)
+2. [Description](#Description)
+3. [Installation](#Installation)
+4. [Usage](#Usage)
+5. [License](#License)
+6. [Contributing](#Contributing)
+7. [Tests](#Tests)
+8. [Questions?](#Questions?)
+  
+## Installation
+installation
+  
+## Usage
+usage
+  
+## License [![License: Apache2.0](https://img.shields.io/badge/License-Apache2.0-yellow.svg)](https://opensource.org/licenses/Apache2.0)
+This application is covered under the [Apache2.0 License](https://opensource.org/licenses/Apache2.0)
+  
+## Contributing
+contributions
+## Tests
+tests
+  
+## Questions?
+Email me at me@me.com
+Check out my [GitHub Profile](https://github.com/me) 
     
--- readme.js
+++ readme.js
@@ -47,7 +47,7 @@ function promptUser() {
             choices: [
                 "ISC",
                 "MIT",
-                "Apache-2.0"
+                "Apache2.0"
             ]
         },
         {
@@ -67,50 +67,39 @@ function promptUser() {
 }
 
 function generateReadMe(answers) {
-    // let badge = "";
-
-    // if (answers.License === "ISC") {
-    //    let badge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-    // } else if (answers.License === "MIT") {
-    //     let badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-    // } else {
-    //     let badge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-    // }
 
     return `
-      # ${answers.Title}
-
-      ## Description
-      ${answers.Description}
-      
-      ## Table of Contents
-      1. [Title](#Title)
-      2. [Description](#Description)
-      3. [Installation](#Installation)
-      4. [Usage](#Usage)
-      5. [License](#License)
-      6. [Contributing](#Contributing)
-      7. [Tests](#Tests)
-      8. [Questions?](#Questions?)
-      
-      ## Installation
-      ${answers.Installation}
-      
-      ## Usage
-      ${answers.Usage}
-      
-      ## License "[![License: ${answers.License}](https://img.shields.io/badge/License-${answers.License}-yellow.svg)](https://opensource.org/licenses/${answers.License})";
-      This application is covered under the [${answers.License} License](https://opensource.org/licenses/${answers.License})
-      
-      ## Contributing
-      ${answers.Contributing}
-
-      ## Tests
-      ${answers.Tests}
-      
-      ## Questions?
-      Email me at ${answers.Email}
-      Check out my [GitHub Profile](https://github.com/${answers.GitHub}) 
+# ${answers.Title}
+## Description
+${answers.Description}
+  
+## Table of Contents
+1. [Title](#Title)
+2. [Description](#Description)
+3. [Installation](#Installation)
+4. [Usage](#Usage)
+5. [License](#License)
+6. [Contributing](#Contributing)
+7. [Tests](#Tests)
+8. [Questions?](#Questions?)
+  
+## Installation
+${answers.Installation}
+  
+## Usage
+${answers.Usage}
+  
+## License [![License: ${answers.License}](https://img.shields.io/badge/License-${answers.License}-yellow.svg)](https://opensource.org/licenses/${answers.License})
+This application is covered under the [${answers.License} License](https://opensource.org/licenses/${answers.License})
+  
+## Contributing
+${answers.Contributing}
+## Tests
+${answers.Tests}
+  
+## Questions?
+Email me at ${answers.Email}
+Check out my [GitHub Profile](https://github.com/${answers.GitHub}) 
     `;
 }
 
